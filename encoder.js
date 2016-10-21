c95470a10b1b99b2685bf311e28d6004b1759dd5
Jenna Lou <jennalouronquillo@gmail.com>
2016-10-20 21:27:45 -0700

    caesar alphabet

--- encoder.js
+++ encoder.js
@@ -6,8 +6,54 @@ $("#inputText").keyup(function(e) {
 $("input[type='radio']").click(function() {
   $("input:checked").prop('checked', false);
   $(this).prop('checked', true );
+  document.getElementById("inputText").value = "";
+  document.getElementById("textArea").textContent = "";
 });
 
-if ($("input:checked").val() == "echo") {
+if ($("#inputText").keydown(function(e){
+var keyThing = e.keyCode;
 
+var bleh = redoneObject[keyThing];
+if($("input:checked").val()  == "echo") {
+   $("#textArea").append(redoneObject[keyThing]);
+}
+else if($("input:checked").val()  == "caesar") {
+   $("#textArea").append(caesarAlphabet[bleh]);
+}
+else if($("input:checked").val()  == "heiroglyphis") {
+    $("#textArea").append(heiroglyphicsThing[bleh]);
+}
+else if($("input:checked").val()  == "runes") {
+  $("#textArea").append(runeThibg[bleh]);
+   }
+
+}
+
+var caesarAlphabet = {
+  "a": "z",
+  "b": "a",
+  "c": "b",
+  "d": "c",
+  "e": "d",
+  "f": "e",
+  "g": "f",
+  "h": "g",
+  "i": "h",
+  "j": "i",
+  "k": "j",
+  "l": "k",
+  "m": "l",
+  "n": "m",
+  "o": "n",
+  "p": "o",
+  "q": "p",
+  "r": "q",
+  "s": "r",
+  "t": "s",
+  "u": "t",
+  "v": "u",
+  "w": "v",
+  "x": "w",
+  "y": "x",
+  "z": "y",
 }
